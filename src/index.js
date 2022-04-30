a5832100241124b009ef2b5e0fe2500c47931451
SergeiAnikeev <anikeevsu@gmail.com>
2022-04-30 20:02:36 +1200

    Changes with style and moving components

--- src/index.js
+++ src/index.js
@@ -6,12 +6,6 @@ function Booklist() {
   return (
     <section className="booklist">
       <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
     </section>
   );
 }
@@ -19,22 +13,31 @@ function Booklist() {
 const Book = () => {
   return (
     <article className="book">
-      <Image></Image>
+    <img
+      src="https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX482_BO1,204,203,200_.jpg"
+      alt="hukl"
+      />
       <Title />
       <Author />
     </article>
   );
 };
 
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX482_BO1,204,203,200_.jpg"
-    alt="hukl"
-  />
-);
+const Title = () => {
+  const title = 'I Love You to the Moon and Back'
+  return (
+    <h1>{title}</h1>
+  );
+ }
 
-const Title = () => <h1>I Love You to the Moon and Back</h1>;
-
-const Author = () => <h4>Amelia Hepworth</h4>;
+const Author = () => {
+  const autor = 'Amelia Hepworth';
+  return(
+    // stronger than .css file
+    <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
+      {autor.toUpperCase()}
+    </h4>
+  )
+}
 
 ReactDOM.render(<Booklist />, document.getElementById("root"));
