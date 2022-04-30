186238cb8f68c0f5a84904c199b3ec6f08010794
SergeiAnikeev <anikeevsu@gmail.com>
2022-04-30 20:31:48 +1200

    Destructuring props

--- src/index.js
+++ src/index.js
@@ -2,42 +2,59 @@ import ReactDOM from "react-dom";
 import React from "react";
 import './index.css';
 
+//setup vars
+const firstBook = {
+  image: "https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX482_BO1,204,203,200_.jpg",
+  title: "I Love You to the Moon and Back",
+  autor: "Amelia Hepworth"
+};
+
+const secondBook = {
+  image:
+    "https://images-na.ssl-images-amazon.com/images/I/51x8pmqjY0L._SX376_BO1,204,203,200_.jpg",
+  title: "Oh, the Places You'll Go!",
+  autor: "Dr. Seuss",
+};
+
 function Booklist() {
   return (
     <section className="booklist">
-      <Book />
+      <Book
+        image={firstBook.image}
+        title={firstBook.title}
+        author={firstBook.autor}
+      />
+      <Book
+        image={secondBook.image}
+        title={secondBook.title}
+        author={secondBook.autor}
+      />
     </section>
   );
-}
+};
 
-const Book = () => {
+const Book = ({ image, title, author }) => {
+  //const { image, title, author } = props;
   return (
     <article className="book">
-    <img
-      src="https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX482_BO1,204,203,200_.jpg"
-      alt="hukl"
-      />
-      <Title />
-      <Author />
+      <img src={image} alt="Book cover" />
+      <Title bookTitle={title} />
+      <Author bookAuthor={author} />
     </article>
   );
 };
 
-const Title = () => {
-  const title = 'I Love You to the Moon and Back'
-  return (
-    <h1>{title}</h1>
-  );
- }
+const Title = ({ bookTitle }) => {
+  return <h1>{bookTitle}</h1>;
+};
 
-const Author = () => {
-  const autor = 'Amelia Hepworth';
-  return(
+const Author = ({ bookAuthor }) => {
+  return (
     // stronger than .css file
     <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
-      {autor.toUpperCase()}
+      {bookAuthor}
     </h4>
-  )
-}
+  );
+};
 
 ReactDOM.render(<Booklist />, document.getElementById("root"));
