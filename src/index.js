e7c68c4d06595fba94bcc79ea43576f4f0b834c9
SergeiAnikeev <anikeevsu@gmail.com>
2022-04-30 17:51:36 +1200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom";
+import React from "react";
+
+function Booklist() {
+  return <section>this is a booklist</section>;
+}
+
+ReactDOM.render(<Booklist />, document.getElementById("root"));
